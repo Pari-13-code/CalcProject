b3d099e118d776d64bb5d97d60ee205041282b0f
ps9102-hub <ps9102@srmist.edu.in>
2026-03-16 19:37:38 +0530

    update code

--- Calc.c
+++ Calc.c
@@ -1,19 +1,45 @@
 #include <stdio.h>
 
 int main() {
-    int a;
-    int b;
+    int choice;
+    int a, b;
 
-    printf("Enter first number:");
-    scanf("%d", &a);
-    printf("Enter second number:");
-    scanf("%d", &b);
+    while (1) {
+        printf("\n---- Calculator Menu ----\n");
+        printf("1. Addition\n");
+        printf("2. Subtraction\n");
+        printf("3. Multiplication\n");
+        printf("4. Division\n");
+        printf("5. Exit\n");
 
-    printf("Addition: %d\n", a+b);
-    printf("Subtraction: %d\n", a-b);
-    printf("Multiplication: %d\n", a*b);
-    printf("Division: %d\n", a/b); 
-    printf("Modulus: %d", a%b);
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        if (choice == 5) {
+            printf("Exiting program...\n");
+            break;
+        }
+
+        printf("Enter two numbers: ");
+        scanf("%d %d", &a, &b);
+
+        switch(choice) {
+            case 1:
+                printf("Result: %d\n", a + b);
+                break;
+            case 2:
+                printf("Result: %d\n", a - b);
+                break;
+            case 3:
+                printf("Result: %d\n", a * b);
+                break;
+            case 4:
+                printf("Result: %d\n", a / b);
+                break;
+            default:
+                printf("Invalid choice\n");
+        }
+    }
 
     return 0;
 }
